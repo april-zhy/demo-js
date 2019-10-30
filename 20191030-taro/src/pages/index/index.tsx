f2028780d558eebf7534af86f01399783e083114
ZhangHongyu <zhanghongyublog@163.com>
2019-10-30 17:11:19 +0800

    20191030: init welcome page

--- 20191030-taro/src/pages/index/index.tsx
+++ 20191030-taro/src/pages/index/index.tsx
@@ -1,35 +1,65 @@
-import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
 import './index.scss'
+import { View, Text } from '@tarojs/components';
+import Taro, { Component, Config } from '@tarojs/taro';
+import * as loginService from '../../services';
+import { Toast } from '../../components';
+import { ZToast } from '../../components/toast/z-toast';
 
-export default class Index extends Component {
+class Index extends Component {
 
-  /**
-   * 指定config的类型声明为: Taro.Config
-   *
-   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
-   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
-   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
-   */
   config: Config = {
-    navigationBarTitleText: '首页'
+    navigationBarBackgroundColor: "#5683F8",
   }
 
-  componentWillMount () { }
+  state = {
+    succeed: true
+  }
 
-  componentDidMount () { }
+  async componentDidMount() {
+    Taro.showLoading({
+      title: '自动登录中...',
+      mask: true
+    });
+    const result = await loginService.getToken();
+    Taro.hideLoading();
+    if (result.error_no) {
+      if (result.error_no === 1003) {
+        // 跳转至登录页面
+      }
+      if (result.error_no === 1000) {
+        ZToast.error({
+          title: result.error_msg,
+          complete: () => {
+            this.setState({
+              succeed: false
+            });
+          }
+        });
+      } else {
+        ZToast.error({
+          title: result.error_msg
+        });
+      }
+    } else {
+      ZToast.success({
+        title: '登录成功！',
+        complete: () => {
+          Taro.redirectTo({
+            url: '../apps/index'
+          });
+        }
+      });
+    }
+  }
 
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
-  render () {
+  render() {
     return (
-      <View className='index'>
-        <Text>Hello world!</Text>
+      <View className={this.state.succeed ? 'wrapper successBg' : 'wrapper errorBg'} >
+        <Text className='welcome'>欢迎使用 ITTI 助手</Text>
+        <Toast></Toast>
       </View>
     )
   }
 }
+
+export default Index;
