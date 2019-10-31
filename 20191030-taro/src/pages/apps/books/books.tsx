82fe52477455c656fc67064556f9c99e9ccd62b7
ZhangHongyu <zhanghongyublog@163.com>
2019-10-31 11:35:14 +0800

    20191031: rich books

--- 20191030-taro/src/pages/apps/books/books.tsx
+++ 20191030-taro/src/pages/apps/books/books.tsx
@@ -1,11 +1,11 @@
 import './books.scss'
 import Taro, { Component, Config } from '@tarojs/taro';
-import { View, Text, Image, ScrollView, Button } from '@tarojs/components';
+import { View, Text, Image, ScrollView } from '@tarojs/components';
 
 import { Scroll, Toast } from '../../../components';
+import { ZToast } from '../../../components/toast/z-toast';
 import { BookResponse, BookTypeResponse } from 'src/@types';
 import * as bookService from '../../../services/service/book.service';
-import { ZToast } from '../../../components/toast/z-toast';
 
 
 class Books extends Component {
@@ -13,26 +13,27 @@ class Books extends Component {
   config: Config = {
     navigationBarTitleText: '书籍管理'
   }
-  extras = {
-
-  }
   state = {
-    activeTypeId: 1,
+    activeTypeId: 0,
     extras: {
-      noMoreMsg: '已经加载完成~',
+      noMoreMsg: '~已经加载完成~',
       noDataMsg: '暂无该类型的书籍'
     },
     params: {
-      type_id: 1,
       page_size: 75
     },
-    typeList: [],
+    typeList: [{
+      bookTypeId: 0,
+      typeName: '全部'
+    }],
     bookList: [],
   }
 
   onScroll(e) {
     if (e.error_no) {
-      // show error msg
+      ZToast.error({
+        title: e.error_msg
+      });
     } else {
       this.setState({
         bookList: bookService.formatBookList(e)
@@ -47,66 +48,70 @@ class Books extends Component {
   onTypeChange(typeId) {
     this.setState({
       activeTypeId: typeId,
-      params: {
-        type_id: typeId,
-        page_size: 75
-      }
     });
-  }
-
-  async getBookTypes() {
-    const _result = await bookService.getBookTypes();
-    if (_result.error_no) {
-      // show error msg
+    if (typeId !== 0) {
+      this.setState({
+        params: {
+          type_id: typeId,
+          page_size: 75
+        }
+      });
     } else {
       this.setState({
-        typeList: _result
+        params: { page_size: 75 }
       });
     }
   }
 
-  onClick() {
-    // ZToast.success({
-    //   title: '正确消息'
-    // });
-    // ZToast.info({
-    //   title: '提示消息'
-    // });
-    // ZToast.success({
-    //   title: '111消息',
-    //   complete: () => {
-    //     console.error('toast complete');
-    //   },
-    //   duration: 500
-    // });
-
-    ZToast.warning({
-      title: '警告消息'
-    });
+  async getBookTypes() {
+    Taro.showLoading();
+    const _result = await bookService.getBookTypes();
+    Taro.hideLoading();
+    if (_result.error_no) {
+      ZToast.error({
+        title: _result.error_msg
+      });
+    } else {
+      const prevTypeList = this.state.typeList;
+      this.setState({
+        typeList: prevTypeList.concat(_result)
+      });
+    }
   }
 
-
   render() {
-
+    const { activeTypeId, extras, params, typeList, bookList } = this.state;
     return (
       <View className='container'>
-        <Button onClick={this.onClick.bind(this)}>button </Button>
         <View className="header">
-          <ScrollView scrollX className="scroll-x contentFontSize contentFontColor">
-            {
-              this.state.typeList.map((typeItem: BookTypeResponse) => {
-                return <View className={this.state.activeTypeId === typeItem.bookTypeId ? 'bookItem active' : 'bookItem'} >
-                  <Text className='bookItemName labelFontSize' onClick={this.onTypeChange.bind(this, typeItem.bookTypeId)}>{typeItem.typeName}</Text>
-                </View>
-              })
-            }
+          <View className="subHeader" >
+            <View className='at-icon at-icon-user'></View>
+            <View className="search">
+              <View className='at-icon at-icon-search searchIcon'></View>
+              输入名称查找书籍
+            </View>
+            <View className='z-icon z-icon-scan'></View>
+          </View>
+          <ScrollView scrollX className="scroll-x">
+            {typeList.map((typeItem: BookTypeResponse) => {
+              return <View className={activeTypeId === typeItem.bookTypeId ? 'typeItem active' : 'typeItem'} >
+                <Text className='typeName' onClick={this.onTypeChange.bind(this, typeItem.bookTypeId)}>
+                  {typeItem.typeName}
+                </Text>
+              </View>
+            })}
           </ScrollView>
         </View >
         <View className="content">
-          <Scroll baseUrl='books' extras={this.state.extras} params={this.state.params} onScroll={this.onScroll.bind(this)}>
-            {this.state.bookList.map((book: BookResponse) => {
-              return <View className='bookItem'>
-                <Image className='bookItemImg' src={book.coverThumbnailUrl}></Image>
+          <Scroll baseUrl='books' extras={extras} params={params} onScroll={this.onScroll.bind(this)}>
+            {bookList.map((book: BookResponse) => {
+              let cover;
+              if (book.coverThumbnailUrl !== '' && book.coverThumbnailUrl !== null) {
+                cover = <Image className='bookItemImg' src={book.coverThumbnailUrl}></Image>
+              } else {
+                cover = <View className="noCover" >暂无封面</View>
+              }
+              return <View className='bookItem'> {cover}
                 <Text className='bookItemName labelFontSize'>{book.bookName}</Text>
               </View>
             })}
@@ -118,3 +123,4 @@ class Books extends Component {
   }
 }
 export default Books;
+
